7e48f055946c1f874afb8f6de3bb1c97c0a7b7de
Babar <babarsmoke24@gmail.com>
2025-07-10 21:45:23 +0530

    feat : LogoTicker Intialized

---
+++ src/Components/LogoTicker.tsx
@@ -0,0 +1,24 @@
+import acme from "../assets/logo-acme.png";
+import apex from "../assets/logo-apex.png";
+import celestial from "../assets/logo-celestial.png";
+import echo from "../assets/logo-echo.png";
+import pulse from "../assets/logo-pulse.png";
+import { motion } from "framer-motion";
+import quantum from "../assets/logo-quantum.png";
+
+const LogoTicker = () => {
+  return (
+    <section className=" text-white relative text-center min-h-screen  mt-8">
+      <div>
+        <h1 className="text-5xl">Trusted by Top Companies</h1>
+        <p className="text-lg my-6">
+          Millions of People get places and find their paths Lorem ipsum dolor
+          sit amet.
+        </p>
+      </div>
+      <div></div>
+    </section>
+  );
+};
+
+export default LogoTicker;
