a8d455e6af2aef38ee1657ecc59d32b68dd179ba
Babar <babarsmoke24@gmail.com>
2025-08-17 19:37:20 +0530

    feat : REsume Component created

---
+++ src/Components/Resume.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const Resume = () => {
+  const [fullName, setFullName] = useState("");
+  return (
+    <section className="text-white">
+      <div>HOla</div>
+    </section>
+  );
+};
+
+export default Resume;
